372aa4e78d7c8b40d3e5bcb4c5313f82154d00d5
RLant <rebeccalant@gmail.com>
2020-07-12 17:01:33 -0400

    pg 14 lesson 2

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,13 +1,10 @@
-const path = require('path')
-const webpack = require('webpack')
-const HtmlWebPackPlugin = require("html-webpack-plugin")
-const { CleanWebpackPlugin } = require('clean-webpack-plugin')
-
+const path = require("path")
+const webpack = require("webpack")
+const HTMLWebPackPlugin = require("html-webpack-plugin")
 module.exports = {
+  mode: "development",
+  devtool: "source-map",
   entry: './src/client/index.js',
-  mode: 'development',
-  devtool: 'source-map',
-  stats: 'verbose',
   module: {
       rules: [
           {
@@ -18,18 +15,10 @@ module.exports = {
       ]
   },
   plugins: [
-      new HtmlWebPackPlugin({
-          template: "./src/client/views/index.html",
-          filename: "./index.html",
-      }),
-      new CleanWebpackPlugin({
-          // Simulate the removal of files
-          dry: true,
-          // Write Logs to Console
-          verbose: true,
-          // Automatically remove all unused webpack assets on rebuild
-          cleanStaleWebpackAssets: true,
-          protectWebpackAssets: false
+      new HTMLWebPackPlugin({
+        template: "./src/client/views/index.html",
+        filename: "./index.html"
       })
   ]
+
 }
